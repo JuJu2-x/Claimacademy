239f44a3e58ea7d9af1015038810b371a9e0104c
Julius ruo <Juliusruo@gmail.com>
2024-08-19 20:22:14 -0500

    submit homework1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string aboutme = "This is a string variable called aboutme.";
+            string Moreinfo = "Hi my name is Julius ruo and I hope I learn how to code.";
+
+            
+          
+
+        
+
+            
+            Console.WriteLine(aboutme);
+            Console.WriteLine(Moreinfo);
+        }
+    }
+}
